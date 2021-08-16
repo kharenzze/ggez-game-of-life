a3a844961246081d1a0187f232433b6c6f9c77bd
Paolo <str237@gmail.com>
2021-08-16 23:03:35 +0200

    Some traits

--- src/core/grid.rs
+++ src/core/grid.rs
@@ -1,16 +1,28 @@
 pub struct Grid {
   dim: GridDimensions,
-  matrix: Vec<Vec<Cell>>
-}
-pub struct Cell {
-  state: CellState
+  matrix: Vec<Vec<Cell>>,
 }
 
+#[derive(Debug)]
+pub struct Cell {
+  state: CellState,
+}
+
+impl Default for Cell {
+  fn default() -> Self {
+    Self {
+      state: CellState::Dead,
+    }
+  }
+}
+
+#[derive(Debug)]
 enum CellState {
   Dead,
-  Alive
+  Alive,
 }
 
+#[derive(Default, Debug)]
 pub struct GridDimensions {
   x: usize,
   y: usize,
