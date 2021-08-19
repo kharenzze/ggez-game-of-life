73c0396426683add0c582025139fb8bbe82911a2
Paolo <str237@gmail.com>
2021-08-19 19:25:57 +0200

    Resolutions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use ggez::*;
 use ggez_gol::game::MainState;
 
 const RES_1080: Vec2 = const_vec2!([1920.0, 1080.0]);
+const RES_720: Vec2 = const_vec2!([1280.0, 720.0]);
 
 const TITLE: &str = "Title";
 const AUTHOR: &str = "Author";
@@ -20,7 +21,11 @@ fn main() {
 
   info!("Start!");
   let window_setup = ggez::conf::WindowSetup::default().title(TITLE);
-  let res = RES_1080;
+  let res = if cfg!(windows) {
+    RES_720
+  } else {
+    RES_1080
+  };
   let window_mode = ggez::conf::WindowMode::default()
     .min_dimensions(res.x, res.y)
     .dimensions(res.x, res.y);
